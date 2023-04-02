8d184a7a1a48a2c7d85dcea5b9be8b0fabad0438
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-04-02 22:25:34 +0100

    Reverted home page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,14 +9,16 @@ export default function HeroSection() {
 	};
 
 	return (
-		<section className="w-screen max-w-none border-b border-b-neutral-900 px-0 pt-2">
+		<section className="w-screen max-w-screen border-b border-b-neutral-900 px-0 pt-2">
 			<div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 2xl:gap-10 px-4 lg:px-8">
 				<div className="text-center lg:text-left flex flex-col gap-4 items-center lg:items-start lg:gap-5 self-center px-2">
-					<div className="border-2 border-amber-600 flex items-center gap-1 p-1.5 rounded-3xl">
-						<p className="text-xs text-amber-600 font-semibold tracking-wide select-none px-2">Public Beta</p>
-					</div>
 					<h1 className="lg:w-full leading-snug lg:leading-tight text-[2.75rem] md:text-6xl xl:text-7xl font-bold text-transparent bg-gradient-to-t bg-clip-text from-zinc-50/70 to-white">The way to own your library.</h1>
-					<p className="text-sm lg:text-base text-neutral-600">Reda allows you to import, organise, share and read your books, articles, and other digital documents anytime, anywhere.</p>
+					<p className="text-sm lg:text-base text-neutral-600">
+						Reda allows you to import, organize, share and read your books, articles, and other digital documents anytime, anywhere. See status{" "}
+						<a href="https://trulyao.dev/blog/what-about-reda" className="underline decoration-dashed underline-offset-2">
+							here
+						</a>
+					</p>
 					<div className="w-max relative">
 						<button onClick={toggleDownloadOptions}>Try it out now</button>
 						{showDownloadOptions && (
